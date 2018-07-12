fbb716247e54136c28efc439d3b162d3a4463bfa
Ochanda <lewiscrafton95@gmail.com>
2018-07-13 01:15:06 +0300

    minor changes

---
+++ app/src/main/java/com/montylee/bullseyetips/FireIDService.java
@@ -0,0 +1,10 @@
+package com.montylee.bullseyetips;
+
+import com.google.firebase.iid.FirebaseInstanceIdService;
+
+public class FireIDService extends FirebaseInstanceIdService {
+    @Override
+    public void onTokenRefresh() {
+
+    }
+}
